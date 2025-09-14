8cb9c6939d0fbd930184ae4799ebdf342cf83030
ScreenSHOT <146810623+diskordBot@users.noreply.github.com>
2025-09-14 20:02:43 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SERVER_CONFIG = {
-    "host": os.getenv("SERVER_HOST", "192.168.0.106"),
+    "host": os.getenv("SERVER_HOST", "185.72.144.22"),
     "port": int(os.getenv("SERVER_PORT", 8000)),
     "database_url": os.getenv("DATABASE_URL", "decanat_app.db"),
     "backup_enabled": os.getenv("BACKUP_ENABLED", "true").lower() == "true",
